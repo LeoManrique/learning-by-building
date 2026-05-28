1160f2b373d154e84360b2a4849c889f7bfae5a7
Leonardo Manrique <leonardomanrique43@gmail.com>
2026-05-28 05:52:32 +0200

    add initial args
    i guess

--- 40-unit-converter/unit-converter-rust/src/main.rs
+++ 40-unit-converter/unit-converter-rust/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(about = "Convert a value from a unit to another")]
+struct Args {
+    #[arg(long)]
+    value: i32,
+
+    #[arg()]
+    from: String,
+
+    #[arg()]
+    to: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
